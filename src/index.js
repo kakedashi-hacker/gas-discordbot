4806497e1955ade44bb6a2937cd97087555e3726
Yuki Hirai <yukihirai0505@gmail.com>
2018-12-28 10:44:40 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import moment from 'moment'
+import { getProperty } from './props'
+
+function pad(num, size) {
+  let s = `${num}`
+  while (s.length < size) s = `0${s}`
+  return s
+}
+
+global.hackerank = () => {
+  const startDate = moment('2018-12-26')
+  const today = moment()
+  let eulerNum = today.diff(startDate, 'days') + 1
+  if (eulerNum > 255) {
+    eulerNum -= 255
+  }
+  const url = `https://www.hackerrank.com/contests/projecteuler/challenges/euler${pad(eulerNum, 3)}`
+  UrlFetchApp.fetch(getProperty('DISCORD_WEB_HOOK_URK'), {
+    method: 'post',
+    payload: {
+      content: `Today's Hackerrank Project Euler\n${url}`
+    }
+  })
+}
